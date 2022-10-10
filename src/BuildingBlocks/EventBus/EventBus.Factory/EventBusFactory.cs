52882ce0bc820aa9b49b5d4b4de856f9db893bd2
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-10-10 23:45:04 +0300

    added factory static class

---
+++ src/BuildingBlocks/EventBus/EventBus.Factory/EventBusFactory.cs
@@ -0,0 +1,19 @@
+using EventBus.AzureServiceBus;
+using EventBus.Base;
+using EventBus.Base.Abstraction;
+using EventBus.RabbitMQ;
+
+namespace EventBus.Factory;
+
+public static class EventBusFactory
+{
+    public static IEventBus Create(EventBusConfig config, IServiceProvider serviceProvider)
+    {
+        //var conn = new DefaultServiceBusPersisterConnection(config);
+        return config.EventBusType switch
+        {
+            EventBusType.AzureServiceBus => new EventBusServiceBus(config, serviceProvider),
+            _ => new EventBusRabbitMQ(config, serviceProvider),
+        };
+    }
+}
