2786f3642c2b42946437d867b3d9e6fe6e69149e
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-10-10 21:30:43 +0300

    added IDisposable

--- src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBus.cs
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBus.cs
@@ -2,7 +2,7 @@ using EventBus.Base.Event;
 
 namespace EventBus.Base.Abstraction;
 
-public interface IEventBus
+public interface IEventBus : IDisposable
 {
     void Publish(IntegrationEvent @event);
     void Subscribe<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
