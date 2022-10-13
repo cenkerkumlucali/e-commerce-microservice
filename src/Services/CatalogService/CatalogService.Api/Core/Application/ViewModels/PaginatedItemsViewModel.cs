683cf0fca992207b23fb0274cead15e25363663c
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-10-13 23:56:24 +0300

    crate PaginatedItemsViewModel

---
+++ src/Services/CatalogService/CatalogService.Api/Core/Application/ViewModels/PaginatedItemsViewModel.cs
@@ -0,0 +1,17 @@
+namespace CatalogService.Api.Core.Application.ViewModels;
+
+public class PaginatedItemsViewModel<TEntity> where TEntity : class
+{
+    public int PageIndex { get; private set; }
+    public int PageSize { get; private set; }
+    public long Count { get; private set; }
+    public IEnumerable<TEntity> Data { get; set; }
+
+    public PaginatedItemsViewModel(int pageIndex, int pageSize, long count, IEnumerable<TEntity> data)
+    {
+        PageIndex = pageIndex;
+        PageSize = pageSize;
+        Count = count;
+        Data = data;
+    }
+}
