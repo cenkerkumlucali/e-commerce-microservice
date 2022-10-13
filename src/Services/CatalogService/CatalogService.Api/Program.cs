0e2720e80d81cc23f22510bb840b120da9d0d552
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-10-14 00:58:03 +0300

    update

--- src/Services/CatalogService/CatalogService.Api/Program.cs
+++ src/Services/CatalogService/CatalogService.Api/Program.cs
@@ -1,20 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
+using CatalogService.Api.Extensions;
+using CatalogService.Api.Infrastructure;
+using CatalogService.Api.Infrastructure.Context;
 
-// Add services to the container.
+var webApplicationOptions = new WebApplicationOptions
+{
+    Args = args,
+    WebRootPath = "pics",
+    ContentRootPath = Directory.GetCurrentDirectory()
+};   
+var builder = WebApplication.CreateBuilder(webApplicationOptions);
+
+builder.Services.Configure<CatalogSettings>(builder.Configuration.GetSection("CatalogSettings"));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureDbContext(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.MigrateDbContext<CatalogContext>((context, services) =>
+{
+    var env = services.GetRequiredService<IWebHostEnvironment>();
+    var logger = services.GetRequiredService<ILogger<CatalogContextSeed>>();
+
+    new CatalogContextSeed()
+        .SeedAsync(context, env, logger)
+        .Wait();
+});
 
 app.UseHttpsRedirection();
 
