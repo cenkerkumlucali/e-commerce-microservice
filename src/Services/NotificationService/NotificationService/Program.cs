e2d299604c2282e07cc05fab697c7c1f15ccf713
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-10-17 23:38:57 +0300

    create Notification service Program.cs

--- src/Services/NotificationService/NotificationService/Program.cs
+++ src/Services/NotificationService/NotificationService/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using EventBus.Base;
+using EventBus.Base.Abstraction;
+using EventBus.Factory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NotificationService.IntegrationEvents.EventHandlers;
+using NotificationService.IntegrationEvents.Events;
+
+ServiceCollection serviceCollection = new ServiceCollection();
+ConfigureServices(serviceCollection);
+var sp = serviceCollection.BuildServiceProvider(); 
+IEventBus eventBus = sp.GetRequiredService<IEventBus>();
+
+eventBus.Subscribe<OrderPaymentFailedIntegrationEvent, OrderPaymentFailedIntegrationEventHandler>();
+eventBus.Subscribe<OrderPaymentSuccessIntegrationEvent, OrderPaymentSuccessIntegrationEventHandler>();
+
+Console.WriteLine("Application is Running....");
+Console.ReadLine();
+
+static void ConfigureServices(ServiceCollection services)
+{
+    services.AddLogging(configure => configure.AddConsole());
+    services.AddTransient<OrderPaymentFailedIntegrationEventHandler>();
+    services.AddTransient<OrderPaymentSuccessIntegrationEventHandler>();
+
+    services.AddSingleton<IEventBus>(sp =>
+        EventBusFactory.Create(new()
+        {
+            ConnectionRetryCount = 5,
+            EventNameSuffix = "IntegrationEvent",
+            SubscriberClientAppName = "NotificationService",
+            EventBusType = EventBusType.RabbitMQ
+        }, sp)
+    );
+}
